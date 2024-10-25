ca155b52e92d52cca9b1a9c7672a1e652140cc60
Irina Klepova <ivklepova23@gmail.com>
2024-10-25 20:39:58 +0400

    добавлена маршрутизация приложения trading_network

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
+    path("trading_network/", include("trading_network.urls", namespace="trading_network")),
 
 ]
