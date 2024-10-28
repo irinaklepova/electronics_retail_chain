e6557233dfecd785e326ce3b157917e0d390f09e
Irina Klepova <ivklepova23@gmail.com>
2024-10-28 18:08:28 +0400

    добавлена модель поставщика

--- trading_network/models.py
+++ trading_network/models.py
@@ -3,11 +3,37 @@ from django.db import models
 from config.settings import NULLABLE
 
 
+class Provider(models.Model):
+    """Модель поставщика"""
+    SUPPLIER_TYPES = (
+        ("factory", "Завод"),
+        ("retail_network", "Розничная сеть"),
+        ("individual_entrepreneur", "Индивидуальный предприниматель"),
+    )
+
+    name = models.CharField(max_length=250, verbose_name="Название")
+    supplier = models.ForeignKey("self", **NULLABLE, on_delete=models.SET_NULL, verbose_name="Поставщик")
+    supplier_type = models.CharField(max_length=40, choices=SUPPLIER_TYPES, verbose_name="Тип поставщика")
+    level = models.IntegerField(max_length=10, verbose_name="Уровень в иерархии")
+    debt = models.DecimalField(max_digits=10, decimal_places=2, **NULLABLE, default=0,
+                               verbose_name="Задолженность перед поставщиком")
+    creation_time = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    def __str__(self):
+        return f"{self.name} {self.supplier_type} {self.level}"
+
+    class Meta:
+        verbose_name = "Поставщик"
+        verbose_name_plural = "Поставщики"
+        ordering = ("name",)
+
+
 class Product(models.Model):
     """Модель продукта"""
     title = models.CharField(max_length=250, verbose_name="Наименование")
     model = models.CharField(max_length=250, **NULLABLE, verbose_name="Модель")
     release_date = models.DateField(**NULLABLE, verbose_name="Дата выхода на рынок")
+    supplier = models.ForeignKey(Provider, on_delete=models.CASCADE, **NULLABLE, verbose_name="Поставщик")
 
     def __str__(self):
         return f"{self.title}"
@@ -25,6 +51,7 @@ class Contact(models.Model):
     city = models.CharField(max_length=100, verbose_name="Город")
     street = models.CharField(max_length=100, **NULLABLE, verbose_name="Улица")
     house_number = models.PositiveIntegerField(max_length=10, **NULLABLE, verbose_name="Номер дома")
+    supplier = models.ForeignKey(Provider, on_delete=models.CASCADE, **NULLABLE, verbose_name="Поставщик")
 
     def __str__(self):
         return f"{self.email} {self.city} {self.country}"
