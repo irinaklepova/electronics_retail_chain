e586bb514dd0b5e27434b0fb08f3ed846fb30710
Irina Klepova <ivklepova23@gmail.com>
2024-10-28 23:07:00 +0400

    добавлен django_filters в INSTALLED_APPS, DEFAULT_FILTER_BACKENDS в REST_FRAMEWORK

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_filters",
 
     "users",
     "trading_network",
@@ -116,6 +117,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
 # Internationalization
