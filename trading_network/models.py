5cf37ba70bbd30ca101c0d4f60b0511868577a0a
Irina Klepova <ivklepova23@gmail.com>
2024-10-28 16:01:56 +0400

    добавлена модель контакта

--- trading_network/models.py
+++ trading_network/models.py
@@ -15,3 +15,21 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+        ordering = ("title",)
+
+
+class Contact(models.Model):
+    """Модель контакта"""
+    email = models.EmailField(max_length=70, verbose_name="Email")
+    country = models.CharField(max_length=200, verbose_name="Страна")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    street = models.CharField(max_length=100, **NULLABLE, verbose_name="Улица")
+    house_number = models.PositiveIntegerField(max_length=10, **NULLABLE, verbose_name="Номер дома")
+
+    def __str__(self):
+        return f"{self.email} {self.city} {self.country}"
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+        ordering = ("city", "country",)
